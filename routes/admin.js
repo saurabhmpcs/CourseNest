316e0a09098e6798a34c27ca7a30aa8cf30f04a1
saurabh mpcs <saurabhmpcs1998@gmail.com>
2025-01-10 14:19:33 +0530

    added admin router

---
+++ routes/admin.js
@@ -0,0 +1,36 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function (req, res) {
+  res.json({
+    message: "signup endpo",
+  });
+});
+
+adminRouter.post("/signin", function (req, res) {
+  res.json({
+    message: "signin endpoint",
+  });
+});
+
+adminRouter.post("/course", function (req, res) {
+  res.json({
+    message: "signup endpo",
+  });
+});
+
+adminRouter.put("/course", function (req, res) {
+  res.json({
+    message: "signup endpo",
+  });
+});
+
+adminRouter.get("/course/bulk", function (req, res) {
+  res.json({
+    message: "signup endpo",
+  });
+});
+
+module.exports = {
+  adminRouter: adminRouter,
+};
