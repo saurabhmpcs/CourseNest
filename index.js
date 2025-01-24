fbac258da67d776c6cffe13c2020619086ab02cf
saurabh mpcs <saurabhmpcs1998@gmail.com>
2025-01-24 11:45:57 +0530

    step-8 added script

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { adminRouter } = require("./routes/admin");
 const app = express();
 
 app.use("/api/v1/user", userRouter);
+
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
